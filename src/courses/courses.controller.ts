a7b4fccb98781a2acbec9187aca9183ebef28e8c
Rafael Almeida <almeidarp.dev@gmail.com>
2024-10-14 10:54:13 -0300

    feat: create courses controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
   findAll() {
     return 'Listagem de cursos';
+  }
+
+  @Get(':id/:name')
+  findOne(@Param('id') id: string) {
+    return `Curso com ID ${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    console.log(body);
+    return `Update course with ID ${id}`;
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  remove(@Param(':id') id: string) {
+    return `Delete course with ID ${id}`;
   }
 }
