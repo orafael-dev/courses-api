2666514538e2149e3acf6c5191c206865a6cf03c
Rafael Almeida <almeidarp.dev@gmail.com>
2024-10-15 11:47:39 -0300

    refactor: Implements Repository and refactor methods to use the specific repository

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,50 +1,53 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Course } from './courses.entity';
+import { Course } from './entities/courses.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CoursesService {
-  private courses: Course[] = [
-    {
-      id: 1,
-      name: 'NestJS',
-      description: 'Curso introdutório de NestJS',
-      tags: ['node.js', 'typescript', 'javascript'],
-    },
-  ];
+  constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+  ) {}
 
-  findAll() {
-    return this.courses;
+  async findAll() {
+    return this.courseRepository.find();
   }
 
-  findOne(id: number) {
-    const course = this.courses.find((course) => course.id === id);
+  async findOne(id: number) {
+    const course = await this.courseRepository.findOne({
+      where: { id },
+    });
     if (!course) {
       throw new NotFoundException(`Course with id ${id} not found`);
     }
     return course;
   }
 
-  create(createCourseDTO: any) {
-    this.courses.push(createCourseDTO);
-    return createCourseDTO;
+  async create(createCourseDTO: any) {
+    const course = this.courseRepository.create(createCourseDTO);
+    await this.courseRepository.save(course);
+    return course;
   }
 
-  update(id: number, updateCourseDTO: any) {
-    const existingCourse = this.findOne(id);
-
-    if (existingCourse) {
-      const index = this.courses.findIndex((course) => course.id === id);
-      this.courses[index] = {
-        id,
-        ...updateCourseDTO,
-      };
+  async update(id: number, updateCourseDTO: any) {
+    const course = await this.courseRepository.preload({
+      ...updateCourseDTO,
+      id,
+    });
+    if (!course) {
+      throw new NotFoundException(`Course with id ${id} not found`);
     }
+    return this.courseRepository.save(course);
   }
 
-  remove(id: number) {
-    const index = this.courses.findIndex((course) => course.id === id);
-    if (index >= 0) {
-      this.courses.splice(index, 1);
+  async remove(id: number) {
+    const course = await this.courseRepository.findOne({
+      where: { id },
+    });
+    if (!course) {
+      throw new NotFoundException(`Course with id ${id} not found`);
     }
+    return this.courseRepository.remove(course);
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from 'src/courses/entities/courses.entity';
 import { DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
@@ -9,7 +10,7 @@ export const dataSourceOptions: DataSourceOptions = {
   username: 'root',
   password: 'root',
   database: 'courses',
-  entities: [],
+  entities: [Course],
   synchronize: true,
 };
 
