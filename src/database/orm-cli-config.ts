de9f23203024d03ce0b7291ff61cf047d7848d7a
Rafael Almeida <almeidarp.dev@gmail.com>
2024-10-18 11:06:59 -0300

    feat: create CoursesTags table and AddCoursesIdToCoursesTagTable migrations

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,16 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1729170595027 } from 'src/migrations/1729170595027-CreateCoursesTable';
 import { CreateTagsTable1729255753806 } from 'src/migrations/1729255753806-CreateTagsTable';
+import { CreateCoursesTagsTable1729256880555 } from 'src/migrations/1729256880555-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1729257240723 } from 'src/migrations/1729257240723-AddCoursesIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1729170595027, CreateTagsTable1729255753806],
+  migrations: [
+    CreateCoursesTable1729170595027,
+    CreateTagsTable1729255753806,
+    CreateCoursesTagsTable1729256880555,
+    AddCoursesIdToCoursesTagsTable1729257240723,
+  ],
 });
